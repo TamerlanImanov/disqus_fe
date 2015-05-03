2622bb862783f422de9a6caddbfda4676e062500
Tamerlan Imanov <tamerlan.imanov@ce.sdu.edu.kz>
2015-05-03 21:11:34 +0600

    refreshBody

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,5 +1,9 @@
 (function() {
-  var checkEmpty, email, foo, name, retrievedObject, user, yourGlobalVariable;
+  var checkEmpty, email, foo, name, retrievedObject, user, yourGlobalVariable,loadData,deleteAllData,counter;
+
+  counter = localStorage.getItem('counter');
+  counter = 0;
+  console.log (counter);
 
   yourGlobalVariable = 0;
 
@@ -36,64 +40,73 @@
       return false;
     }
   };
-  
-  $.ajax({url: 'http://127.0.0.1:8000/api/v1/comment/', 
-    success: function(result){
-      for (var i=0;i<result.objects.length;i++){
-        var contDiv, dd, email, globalDiv, gravatar, img, imgDiv, mesDiv, mesString, mm, nameString, nameUser, newDiv, sep, timeDiv, today, yyyy;
-        mesString = document.getElementById('focusedInput').value;
 
-        globalDiv = document.getElementById('omg');
+  loadData = function(){
+    $.ajax({url: 'http://127.0.0.1:8000/api/v1/comment/', 
+      success: function(result){
+        if (counter<result.objects.length){
+          for (var i=counter+1;i<result.objects.length;i++){
+            var contDiv, dd, email, globalDiv, gravatar, img, imgDiv, mesDiv, mesString, mm, nameString, nameUser, newDiv, sep, timeDiv, today, yyyy;
+            mesString = document.getElementById('focusedInput').value;
 
-        if (yourGlobalVariable > 0) {
-          sep = document.createElement('div');
-          sep.setAttribute('class', 'list-group-separator');
-          globalDiv.appendChild(sep);
+            globalDiv = document.getElementById('omg');
+
+            if (yourGlobalVariable > 0) {
+              sep = document.createElement('div');
+              sep.setAttribute('class', 'list-group-separator');
+              globalDiv.appendChild(sep);
+            }
+
+            newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'list-group-item');
+            globalDiv.appendChild(newDiv);
+
+            imgDiv = document.createElement('div');
+            imgDiv.setAttribute('class', 'row-action-primary');
+            newDiv.appendChild(imgDiv);
+
+            img = document.createElement('img');
+            img.setAttribute('class', 'circle');
+            gravatar = result.objects[i].image;
+            img.setAttribute('src', gravatar);
+            imgDiv.appendChild(img);
+
+            contDiv = document.createElement('div');
+            contDiv.setAttribute('class', 'row-content');
+            newDiv.appendChild(contDiv);
+
+            timeDiv = document.createElement('div');
+            timeDiv.setAttribute('class', 'least-content');
+            var time = result.objects[i].pub_time;
+            time = time.substring (0,10);
+            var day = time.substring (8,10);
+            var month = time.substring (5,7);
+            var year = time.substring (2,4);
+            timeDiv.appendChild(document.createTextNode(day+"."+month+"."+year));
+            contDiv.appendChild(timeDiv);
+
+            nameUser = document.createElement('h4');
+            nameUser.setAttribute('class', 'list-group-item-heading');
+            nameUser.appendChild(document.createTextNode(result.objects[i].author_title));
+            contDiv.appendChild(nameUser);
+
+            mesDiv = document.createElement('p');
+            mesDiv.setAttribute('class', 'list-group-item-text');
+            mesDiv.appendChild(document.createTextNode(result.objects[i].text));
+            contDiv.appendChild(mesDiv);
+
+            yourGlobalVariable++;
+
+            counter = i;
+            localStorage.setItem('counter', counter);
+
+            console.log (counter + " " + i)
+          }
         }
+    }});
+  }
 
-        newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'list-group-item');
-        globalDiv.appendChild(newDiv);
-
-        imgDiv = document.createElement('div');
-        imgDiv.setAttribute('class', 'row-action-primary');
-        newDiv.appendChild(imgDiv);
-
-        img = document.createElement('img');
-        img.setAttribute('class', 'circle');
-        gravatar = result.objects[i].image;
-        img.setAttribute('src', gravatar);
-        imgDiv.appendChild(img);
-
-        contDiv = document.createElement('div');
-        contDiv.setAttribute('class', 'row-content');
-        newDiv.appendChild(contDiv);
-
-        timeDiv = document.createElement('div');
-        timeDiv.setAttribute('class', 'least-content');
-        var time = result.objects[i].pub_time;
-        time = time.substring (0,10);
-        var day = time.substring (8,10);
-        var month = time.substring (5,7);
-        var year = time.substring (2,4);
-        timeDiv.appendChild(document.createTextNode(day+"."+month+"."+year));
-        contDiv.appendChild(timeDiv);
-
-        nameUser = document.createElement('h4');
-        nameUser.setAttribute('class', 'list-group-item-heading');
-        nameUser.appendChild(document.createTextNode(result.objects[i].author_title));
-        contDiv.appendChild(nameUser);
-
-        mesDiv = document.createElement('p');
-        mesDiv.setAttribute('class', 'list-group-item-text');
-        mesDiv.appendChild(document.createTextNode(result.objects[i].text));
-        contDiv.appendChild(mesDiv);
-
-        yourGlobalVariable++;
-
-      }
-  }});
-
+  loadData();
 
   $('.col-lg-10 > input').keyup(function() {
     user.email = document.getElementById('inputEmail').value;
@@ -145,20 +158,7 @@
           processData: false
         })
 
-
-        
-        // today = new Date;
-        // dd = today.getDate();
-        // mm = today.getMonth() + 1;
-        // yyyy = today.getFullYear();
-        // if (dd < 10) {
-        //   dd = '0' + dd;
-        // }
-        // if (mm < 10) {
-        //   mm = '0' + mm;
-        // }
-        // today = dd + '.' + mm + '.' + yyyy;
-        
+        loadData();
         
       }
     }
